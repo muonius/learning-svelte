27f7f1640cc0a43b052da9ea80c1374d0bf10a9d
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-06 20:29:01 -0400

    conditional if

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
   //Svelte requires id for loops
   let people = [
-    { name: "yoshi", beltColour: "black", age: 25, id: 1 },
-    { name: "mario", beltColour: "orange", age: 45, id: 2 },
-    { name: "luigi", beltColour: "brown", age: 35, id: 3 },
+    { name: "yoshi", beltColor: "black", age: 25, id: 1 },
+    { name: "mario", beltColor: "orange", age: 45, id: 2 },
+    { name: "luigi", beltColor: "brown", age: 35, id: 3 },
   ];
 
   const handleClick = (e, id) => {
@@ -31,6 +31,9 @@
   {#each people as person (person.id)}
     <div>
       <h4>{person.name}</h4>
+      {#if person.beltColor === "black"}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
       <p>{person.age} years old, {person.beltColor} belt.</p>
       <!-- here we use backpack to return a function without invoking it -->
       <button
